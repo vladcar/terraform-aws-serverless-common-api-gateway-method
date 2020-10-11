b20c923a8a770deb01faced6360d2776dd60f014
vladc <vlad@aucsys.co.uk>
2020-10-11 19:34:37 +0300

    add x-token header

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   selection_pattern = "-"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Token'",
     "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT,PATCH,DELETE'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
